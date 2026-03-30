ef3ee13b820efc21242149c1c42111b33b6a970a
studentTry6e7e8 <240428754@stu.vtc.edu.hk>
2026-03-30 01:55:30 +0000

    Complete assignment

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 //try to add Hello World into the System.out.println
 public class Main {
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println("Hello World");
   }
 }
